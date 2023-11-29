ea0f3dcfdd8962549d9db690139f4fac4e245718
Mickamar <martasmic@seznam.cz>
2023-11-29 08:54:27 +0100

    komit

--- projekt/src/main.c
+++ projekt/src/main.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
 #include "gpio.h"
+#include "rtc.h"
 
 #define SENZOR_PIN 1 // A1
 #define LAMP 3 // D3
 
 int main() {
+    uint8_t hours, minutes, seconds;
     printf("---- Test fotorezistoru ----\n");
     GPIO_mode_output(&DDRB, LAMP);
-
+    rtc_init();
     while (1) {
         int war = GPIO_read(&PINB, SENZOR_PIN);
         printf("%d\n", war);
-        
+
+        rtc_read_hours(&hours);
+        rtc_read_minutes(&minutes);
+        rtc_read_seconds(&seconds);
+        printf("Aktuální čas: %02d:%02d:%02d\n", hours, minutes, seconds);
+
         if (war > 700) {
             printf("LED svítí\n");
             GPIO_write_high(&PORTB, LAMP);
@@ -19,7 +26,7 @@ int main() {
             GPIO_write_low(&PORTB, LAMP);
         }
         
-        delay(200);
+        _delay_ms(1000);
     }
 
     return 0;
