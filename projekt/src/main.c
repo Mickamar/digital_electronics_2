da7e7c5aaac7760d2d0f53db7aaf6c2391e5846f
Mickamar <martasmic@seznam.cz>
2023-11-29 14:50:45 +0100

    komit

--- projekt/src/main.c
+++ projekt/src/main.c
@@ -1,29 +1,66 @@
 #include <stdio.h>
 #include "gpio.h"
 #include "rtc.h"
+#include "uart.h"
+#include "oled.h"
+#include "twi.h"
+#include <stdlib.h> 
+#include "timer.h"
+#include <avr/io.h>         // AVR device-specific IO definitions
+#include <avr/interrupt.h>
 
-#define SENZOR_PIN 1 // A1
-#define LAMP 3 // D3
+
+
+
+#define SENZOR_PIN PC1 // A1
+#define LAMP PB0 // D3
+
 
 int main() {
+    //char string[8];
+    char time_string[9];
+    char buffer[10];
     uint8_t hours, minutes, seconds;
-    printf("---- Test fotorezistoru ----\n");
-    GPIO_mode_output(&DDRB, LAMP);
-    rtc_init();
-    while (1) {
-        int war = GPIO_read(&PINB, SENZOR_PIN);
-        printf("%d\n", war);
 
+    
+
+    uart_init(UART_BAUD_SELECT(9600, F_CPU));
+    TIM1_OVF_1SEC
+    TIM1_OVF_ENABLE
+    sei();
+    //uart_puts("---- Test fotorezistoru ----\n");
+
+    GPIO_mode_output(&DDRB, LAMP);
+
+    rtc_init();
+
+    while (1) {
+        int war = GPIO_read(&PINC, SENZOR_PIN);
+        itoa(war, buffer,10);
+        uart_puts(buffer);
+        uart_puts("\n");
         rtc_read_hours(&hours);
         rtc_read_minutes(&minutes);
         rtc_read_seconds(&seconds);
-        printf("Aktuální čas: %02d:%02d:%02d\n", hours, minutes, seconds);
 
-        if (war > 700) {
-            printf("LED svítí\n");
-            //GPIO_write_high(&PORTB, LAMP);
-        //} else {
-           // GPIO_write_low(&PORTB, LAMP);
+
+        itoa(hours,time_string,10);
+        uart_puts(time_string);
+        uart_puts(":");
+        itoa(minutes,time_string,10);
+        uart_puts(time_string);
+        uart_puts(":");
+        itoa(seconds,time_string,10);
+        uart_puts(time_string);
+        uart_puts("\n");
+        
+        
+
+        if (war == 0) { //no light, diod shine
+            uart_puts("LED svítí\n");
+            GPIO_write_high(&PORTB, LAMP);
+        } else {
+            GPIO_write_low(&PORTB, LAMP);
         }
         
         _delay_ms(1000);
